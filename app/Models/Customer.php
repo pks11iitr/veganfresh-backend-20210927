8bc0f7bf64ac74e5340cddb5ac237b3ab3ad2d47
pankaj sengar <pks11iitr@gmail.com>
2020-06-20 12:36:18 +0530

    profile view and update completed

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -14,7 +14,7 @@ class Customer extends Authenticatable implements JWTSubject
     protected $table='customers';
 
     protected $fillable = [
-        'name', 'email', 'mobile', 'password', 'image'
+        'name', 'email', 'mobile', 'password', 'image', 'dob','address','city', 'dob'
     ];
 
     protected $hidden = [
