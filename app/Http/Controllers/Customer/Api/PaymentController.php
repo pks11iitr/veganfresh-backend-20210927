0246ae47af0e0930630d95478ead6e47b6947e65
pankaj sengar <pks11iitr@gmail.com>
2020-06-26 18:47:35 +0530

    payment api completed (test pending)

--- app/Http/Controllers/Customer/Api/PaymentController.php
+++ app/Http/Controllers/Customer/Api/PaymentController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\Customer\Api;
 
+use App\Events\OrderSuccessfull;
+use App\Models\Cart;
 use App\Models\Order;
+use App\Models\OrderStatus;
+use App\Models\Therapy;
 use App\Models\Wallet;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -24,14 +28,219 @@ class PaymentController extends Controller
                 'message'=>'Invalid Operation Performed'
             ];
 
+        // set to initial state
+        $order->update([
+            'use_balance'=>false,
+            'use_points'=>false,
+            'points_used'=>0,
+            'balance_used'=>0,
+        ]);
+
+
+
+        if($request->use_points==1) {
+            $result=$this->payUsingPoints($order);
+            if($result['status']=='success'){
+                return [
+                    'status'=>'success',
+                    'message'=>'Congratulations! Your order at Arogyapeeth is successful',
+                    'data'=>[
+                        'payment_done'=>'yes',
+                        'ref_id'=>$order->refid
+                    ]
+                ];
+            }
+        }
+
+        if($request->use_balance==1) {
+            $result=$this->payUsingBalance($order);
+            if($result['status']=='success'){
+                return [
+                    'status'=>'success',
+                    'message'=>'Congratulations! Your order at Arogyapeeth is successful',
+                    'data'=>[
+                        'payment_done'=>'yes',
+                        'ref_id'=>$order->refid
+                    ]
+                ];
+            }
+
+        }
+
+        $result=$this->initiateGatewayPayment($order);
+
+    }
+
+    private function initiateGatewayPayment($order){
+        $response=$this->pay->generateorderid([
+            "amount"=>($order->total-$order->balance_used)*100,
+            "currency"=>"INR",
+            "receipt"=>$order->refid,
+        ]);
+        $responsearr=json_decode($response);
+        if(isset($responsearr->id)){
+            $order->order_id=$responsearr->id;
+            $order->order_id_response=$response;
+            $order->save();
+            return response()->json([
+                'status'=>'success',
+                'message'=>'success',
+                'data'=>[
+                    'paymentdone'=>'no',
+                    'razorpay_order_id'=> $order->order_id,
+                    'total'=>($order->total-$order->balance_used)*100,
+                    'email'=>$order->email,
+                    'mobile'=>$order->mobile,
+                    'description'=>'Order Booking at Aarogyapeeth',
+                    'name'=>$order->name,
+                    'currency'=>'INR',
+                    'merchantid'=>$this->pay->merchantkey,
+                ],
+            ], 200);
+        }else{
+            return response()->json([
+                'status'=>'failed',
+                'message'=>'Payment cannot be initiated',
+                'data'=>[
+                ],
+            ], 200);
+        }
+    }
+
+    private function payUsingBalance($order){
+
+            $walletbalance=Wallet::balance($order->user_id);
+            if($walletbalance<=0)
+                return [
+                    'status'=>'failed',
+                    'remaining_amount'=>$order->total_cost
+                ];
+
+            if($order->total_cost <= $walletbalance) {
+                $order->payment_status='paid';
+                $order->status='confirmed';
+                $order->use_balance=true;
+                $order->balance_used=$order->total_cost*env('POINTS_CONVERSION_RATE');
+                $order->payment_mode='online';
+                $order->save();
+
+                OrderStatus::create([
+                    'order_id'=>$order->id,
+                    'current_status'=>$order->status
+                ]);
+
+                Wallet::updatewallet($order->user_id, 'Paid For Order ID: '.$order->refid, 'DEBIT',$order->balance_used, 'CASH', $order->id);
+
+                Cart::where('user_id', $order->user_id)->delete();
+
+                return [
+                    'status'=>'success',
+                ];
+            }else {
+
+                $order->use_balance=true;
+                $order->balance_used=$walletbalance;
+                $order->payment_mode='online';
+                $order->save();
+
+                return [
+                    'status'=>'failed',
+                ];
+            }
+    }
+
+    private function payUsingPoints($order){
+        //points can be used for therapy only
+
+        if($order->details[0]->entity instanceof Therapy){
+                $walletpoints=Wallet::points($order->user_id);
+                $amount=$walletpoints/env('POINTS_CONVERSION_RATE');
+                if($amount>=$order->total_cost){
+                    $order->payment_status='paid';
+                    $order->status='confirmed';
+                    $order->use_points=true;
+                    $order->points_used=$order->total_cost*env('POINTS_CONVERSION_RATE');
+                    $order->payment_mode='online';
+                    $order->save();
+
+                    OrderStatus::create([
+                        'order_id'=>$order->id,
+                        'current_status'=>$order->status
+                    ]);
+
+                    Wallet::updatewallet($order->user_id, 'Paid For Order ID: '.$order->refid, 'DEBIT',$order->points_used, 'POINT', $order->id);
+
+                    Cart::where('user_id', $order->user_id)->delete();
+
+                    return [
+                            'status'=>'success',
+                        ];
+                }
+            }
+
         return [
-            'status'=>'success',
-            'data'=>[
-                'order_id'=>$order->id,
-                'amount'=>$order->total_cost,
-                'walletbalance'=>Wallet::balance($user->id),
-                'points'=>Wallet::points($user->id)
-            ]
+            'status'=>'failed'
         ];
+    }
+
+
+    public function verifyPayment(Request $request){
+        $order=Order::where('order_id', $request->razorpay_order_id)->first();
+
+        if(!$order)
+            return [
+                'status'=>'failed',
+                'message'=>'Invalid Operation Performed'
+            ];
+
+        $paymentresult=$this->pay->verifypayment($request->all());
+        if($paymentresult){
+            if($order->use_balance==true) {
+                $balance = Wallet::balance($order->user_id);
+                if ($balance < $order->balance_used) {
+                    return response()->json([
+                        'status' => 'failed',
+                        'message' => 'We apologize, Your order is not successful',
+                        'errors' => [
+
+                        ],
+                    ], 200);
+                }
+            }
+            $order->status='confirmed';
+            $order->payment_id=$request->razorpay_payment_id;
+            $order->payment_id_response=$request->razorpay_signature;
+            $order->payment_status='paid';
+            $order->payment_mode='online';
+            $order->save();
+
+            OrderStatus::create([
+                'order_id'=>$order->id,
+                'current_status'=>$order->status
+            ]);
+
+            if($order->balance_used > 0)
+                Wallet::updatewallet($order->user_id, 'Paid For Order ID: '.$order->refid, 'DEBIT',$balance, 'CASH', $order->id);
+
+            Cart::where('user_id', $order->user_id)->delete();
+
+            //event(new OrderSuccessfull($order));
+
+            return [
+                'status'=>'success',
+                'message'=> 'Congratulations! Your order at Arogyapeeth is successful',
+                'data'=>[
+                    'ref_id'=>$order->refid
+                ]
+            ];
+        }else{
+            return [
+                'status'=>'failed',
+                'message'=>'We apologize, Your payment cannot be verified',
+                'data'=>[
+
+                ],
+            ];
+        }
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,10 +8,14 @@ class Order extends Model
 {
     protected $table='orders';
 
-    protected $fillable=[ 'refid', 'total_cost', 'status', 'payment_mode', 'order_details_completed', 'booking_date', 'booking_time', 'user_id', 'name', 'email', 'mobile', 'address', 'lat', 'lang', 'is_instant'];
+    protected $fillable=[ 'refid', 'total_cost', 'status', 'payment_status', 'payment_mode', 'order_details_completed', 'booking_date', 'booking_time', 'user_id', 'name', 'email', 'mobile', 'address', 'lat', 'lang', 'is_instant', 'use_wallet', 'use_points', 'balance_used', 'points_used'];
 
     public function details(){
         return $this->hasMany('App\Models\OrderDetail', 'order_id');
+    }
+
+    public function getOrderDescription(){
+
     }
 
 
