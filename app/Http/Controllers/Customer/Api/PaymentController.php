ba761c877fe5b08aca35a6a609bc703eaa59103f
pankaj sengar <pks11iitr@gmail.com>
2020-06-28 21:08:28 +0530

    testing payment apis

--- app/Http/Controllers/Customer/Api/PaymentController.php
+++ app/Http/Controllers/Customer/Api/PaymentController.php
@@ -196,7 +196,7 @@ class PaymentController extends Controller
     public function verifyPayment(Request $request){
         $order=Order::where('order_id', $request->razorpay_order_id)->first();
 
-        if(!$order)
+        if(!$order || $order->status=='pending')
             return [
                 'status'=>'failed',
                 'message'=>'Invalid Operation Performed'
