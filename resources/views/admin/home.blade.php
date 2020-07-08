064cfdcd8e7b8799441ae5509fc4ae039e001fad
pankaj sengar <pks11iitr@gmail.com>
2020-07-08 13:48:50 +0530

    add sales on dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -534,73 +534,73 @@
                                 <canvas class="chart" id="line-chart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
                             </div>
                             <!-- /.card-body -->
-                            <div class="card-footer bg-transparent">
-                                <div class="row">
-                                    <div class="col-4 text-center">
-                                        <input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60"
-                                               data-fgColor="#39CCCC">
+{{--                            <div class="card-footer bg-transparent">--}}
+{{--                                <div class="row">--}}
+{{--                                    <div class="col-4 text-center">--}}
+{{--                                        <input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60"--}}
+{{--                                               data-fgColor="#39CCCC">--}}
 
-                                        <div class="text-white">Mail-Orders</div>
-                                    </div>
-                                    <!-- ./col -->
-                                    <div class="col-4 text-center">
-                                        <input type="text" class="knob" data-readonly="true" value="50" data-width="60" data-height="60"
-                                               data-fgColor="#39CCCC">
+{{--                                        <div class="text-white">Mail-Orders</div>--}}
+{{--                                    </div>--}}
+{{--                                    <!-- ./col -->--}}
+{{--                                    <div class="col-4 text-center">--}}
+{{--                                        <input type="text" class="knob" data-readonly="true" value="50" data-width="60" data-height="60"--}}
+{{--                                               data-fgColor="#39CCCC">--}}
 
-                                        <div class="text-white">Online</div>
-                                    </div>
-                                    <!-- ./col -->
-                                    <div class="col-4 text-center">
-                                        <input type="text" class="knob" data-readonly="true" value="30" data-width="60" data-height="60"
-                                               data-fgColor="#39CCCC">
+{{--                                        <div class="text-white">Online</div>--}}
+{{--                                    </div>--}}
+{{--                                    <!-- ./col -->--}}
+{{--                                    <div class="col-4 text-center">--}}
+{{--                                        <input type="text" class="knob" data-readonly="true" value="30" data-width="60" data-height="60"--}}
+{{--                                               data-fgColor="#39CCCC">--}}
 
-                                        <div class="text-white">In-Store</div>
-                                    </div>
-                                    <!-- ./col -->
-                                </div>
-                                <!-- /.row -->
-                            </div>
+{{--                                        <div class="text-white">In-Store</div>--}}
+{{--                                    </div>--}}
+{{--                                    <!-- ./col -->--}}
+{{--                                </div>--}}
+{{--                                <!-- /.row -->--}}
+{{--                            </div>--}}
                             <!-- /.card-footer -->
                         </div>
                         <!-- /.card -->
 
                         <!-- Calendar -->
-                        <div class="card bg-gradient-success">
-                            <div class="card-header border-0">
+{{--                        <div class="card bg-gradient-success">--}}
+{{--                            <div class="card-header border-0">--}}
 
-                                <h3 class="card-title">
-                                    <i class="far fa-calendar-alt"></i>
-                                    Calendar
-                                </h3>
-                                <!-- tools card -->
-                                <div class="card-tools">
-                                    <!-- button with a dropdown -->
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52">
-                                            <i class="fas fa-bars"></i></button>
-                                        <div class="dropdown-menu" role="menu">
-                                            <a href="#" class="dropdown-item">Add new event</a>
-                                            <a href="#" class="dropdown-item">Clear events</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a href="#" class="dropdown-item">View calendar</a>
-                                        </div>
-                                    </div>
-                                    <button type="button" class="btn btn-success btn-sm" data-card-widget="collapse">
-                                        <i class="fas fa-minus"></i>
-                                    </button>
-                                    <button type="button" class="btn btn-success btn-sm" data-card-widget="remove">
-                                        <i class="fas fa-times"></i>
-                                    </button>
-                                </div>
-                                <!-- /. tools -->
-                            </div>
-                            <!-- /.card-header -->
-                            <div class="card-body pt-0">
-                                <!--The calendar -->
-                                <div id="calendar" style="width: 100%"></div>
-                            </div>
-                            <!-- /.card-body -->
-                        </div>
+{{--                                <h3 class="card-title">--}}
+{{--                                    <i class="far fa-calendar-alt"></i>--}}
+{{--                                    Calendar--}}
+{{--                                </h3>--}}
+{{--                                <!-- tools card -->--}}
+{{--                                <div class="card-tools">--}}
+{{--                                    <!-- button with a dropdown -->--}}
+{{--                                    <div class="btn-group">--}}
+{{--                                        <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52">--}}
+{{--                                            <i class="fas fa-bars"></i></button>--}}
+{{--                                        <div class="dropdown-menu" role="menu">--}}
+{{--                                            <a href="#" class="dropdown-item">Add new event</a>--}}
+{{--                                            <a href="#" class="dropdown-item">Clear events</a>--}}
+{{--                                            <div class="dropdown-divider"></div>--}}
+{{--                                            <a href="#" class="dropdown-item">View calendar</a>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                    <button type="button" class="btn btn-success btn-sm" data-card-widget="collapse">--}}
+{{--                                        <i class="fas fa-minus"></i>--}}
+{{--                                    </button>--}}
+{{--                                    <button type="button" class="btn btn-success btn-sm" data-card-widget="remove">--}}
+{{--                                        <i class="fas fa-times"></i>--}}
+{{--                                    </button>--}}
+{{--                                </div>--}}
+{{--                                <!-- /. tools -->--}}
+{{--                            </div>--}}
+{{--                            <!-- /.card-header -->--}}
+{{--                            <div class="card-body pt-0">--}}
+{{--                                <!--The calendar -->--}}
+{{--                                <div id="calendar" style="width: 100%"></div>--}}
+{{--                            </div>--}}
+{{--                            <!-- /.card-body -->--}}
+{{--                        </div>--}}
                         <!-- /.card -->
                     </section>
                     <!-- right col -->
