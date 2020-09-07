23f75734619d217bd1f37514e96dc7ce27e939a9
Alam <badre.alam1993@gmail.com>
2020-09-07 18:20:04 +0530

    discount add

--- app/Models/Size.php
+++ app/Models/Size.php
@@ -11,4 +11,9 @@ class Size extends Model
     protected $fillable=['size', 'price','cut_price','product_id', 'isactive'];
 
     protected $hidden =['created_at','updated_at','deleted_at'];
+    protected $appends=['discount'];
+
+    public function getDiscountAttribute($value){
+        return intval((($this->cut_price-$this->price)/$this->cut_price)*100);
+    }
 }
