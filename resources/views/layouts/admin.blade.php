52a021530f30268a6cac4161d61bc3cd0c138026
ravi <ravisaini9900@gmail.com>
2020-09-14 13:01:17 +0530

    add Suzo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Arogya</title>
+    <title>Suzo</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
@@ -83,7 +83,7 @@
         <a href="index3.html" class="brand-link">
            <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">-->
-            <span class="brand-text font-weight-light">Arogyapeeth CMS</span>
+            <span class="brand-text font-weight-light">Suzo CMS</span>
         </a>
 
         <!-- Sidebar -->
@@ -94,7 +94,7 @@
                     <img src="{{\Illuminate\Support\Facades\Storage::url('images/logo.jpeg')}}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">Arogyapeeth</a>
+                    <a href="#" class="d-block">Suzo</a>
                 </div>
             </div>
 
