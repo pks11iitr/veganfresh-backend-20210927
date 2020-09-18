e4b89ba1ef87805628fa247230646afe37b95276
pankaj sengar <pks11iitr@gmail.com>
2020-09-18 18:45:26 +0530

    temporary commit

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -14,7 +14,7 @@ class Order extends Model
      */
     protected $table='orders';
 
-    protected $fillable=[ 'refid', 'total_cost', 'status', 'payment_status', 'payment_mode', 'order_details_completed', 'booking_date', 'booking_time', 'user_id', 'name', 'email', 'mobile', 'address', 'lat', 'lang', 'is_instant', 'use_wallet', 'use_points', 'balance_used', 'points_used','schedule_type','order_place_state'];
+    protected $fillable=[ 'refid', 'total_cost', 'status', 'payment_status', 'payment_mode', 'order_details_completed', 'booking_date', 'booking_time', 'user_id', 'name', 'email', 'mobile', 'address', 'lat', 'lang', 'is_instant', 'use_wallet', 'use_points', 'balance_used', 'points_used','schedule_type','order_place_state','coupon_applied', 'coupon_discount'];
 
     public function details(){
         return $this->hasMany('App\Models\OrderDetail', 'order_id');
@@ -51,52 +51,9 @@ class Order extends Model
 
     public static function getTotal(Order $order){
         $cost=0;
-        if($order->details[0]->entity_type=='App\Models\Therapy')
-        {
-            if($order->details[0]->clinic_id){
 
-                $clinic=Clinic::active()->with(['therapies'=>function($therapies)use($order){
-                    $therapies->where('therapies.isactive', true)->where('therapies.id', $order->details[0]->entity_id);
-                }])->find($order->details[0]->clinic_id);
-
-                $bookings=$order->bookingSlots;
-                foreach($bookings  as $booking){
-                    switch($booking->grade){
-                        case 1:$cost=$cost+($clinic->therapies[0]->pivot->grade1_price??0);
-                            break;
-                        case 2:$cost=$cost+($clinic->therapies[0]->pivot->grade2_price??0);
-                            break;
-                        case 3:$cost=$cost+($clinic->therapies[0]->pivot->grade3_price??0);
-                            break;
-                        case 4:$cost=$cost+($clinic->therapies[0]->pivot->grade4_price??0);
-                            break;
-                    }
-                }
-            }else{
-
-                $therapy=Therapy::find($order->details[0]->entity_id);
-                $bookings=$order->homebookingslots;
-                foreach($bookings  as $booking) {
-                    switch ($booking->grade) {
-                        case 1:
-                            $cost = $cost+($therapy->grade1_price ?? 0);
-                            break;
-                        case 2:
-                            $cost = $cost+($therapy->grade2_price ?? 0);
-                            break;
-                        case 3:
-                            $cost = $cost+($therapy->grade3_price ?? 0);
-                            break;
-                        case 4:
-                            $cost = $cost+($therapy->grade4_price ?? 0);
-                            break;
-                    }
-                }
-            }
-        }else{
-            foreach($order->details as $d){
-                $cost=$cost+$d->entity->price;
-            }
+        foreach($order->details as $d){
+            $cost=$cost+$d->entity->price;
         }
 
         return $cost;
