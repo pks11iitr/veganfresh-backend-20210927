dd4eb8efcb5a916ef85667cda1bd14f06bca64a4
Alam <badre.alam1993@gmail.com>
2020-10-06 19:10:47 +0530

    minor fix in reset otp

--- config/sms-templates.php
+++ config/sms-templates.php
@@ -2,4 +2,5 @@
 return [
     'register'=>'Your OTP is {{otp}} For SuzoDailyNeeds',
     'login'=>'Your OTP is {{otp}} For SuzoDailyNeeds',
+    'reset'=>'Your OTP is {{otp}} For SuzoDailyNeeds',
 ];
