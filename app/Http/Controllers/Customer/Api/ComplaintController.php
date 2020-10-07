3cff621b20c6be26fc698a01a2ed1a36e79e205b
pankaj sengar <pks11iitr@gmail.com>
2020-10-07 15:29:13 +0530

    minor fixes

--- app/Http/Controllers/Customer/Api/ComplaintController.php
+++ app/Http/Controllers/Customer/Api/ComplaintController.php
@@ -112,6 +112,7 @@ class ComplaintController extends Controller
 
         $complaint=Complaint::where('user_id', $user->id)->find($complaint_id);
         $complaint->updated_at=date('Y-m-d H:i:s');
+        $complaint->is_closed=false;
         $complaint->save();
         if(!$complaint)
             return [
