7632b442ab90d17dea7cdfd690997553af59ff29
pankaj sengar <pks11iitr@gmail.com>
2020-10-15 09:58:56 +0530

    download reports

--- app/Http/Controllers/SuperAdmin/StoreController.php
+++ app/Http/Controllers/SuperAdmin/StoreController.php
@@ -22,8 +22,8 @@ class StoreController extends Controller
     public function store(Request $request){
         $request->validate([
             'name'=>'required',
-            'email'=>'required',
-            'mobile'=>'required',
+            'email'=>'required|unique:users',
+            'mobile'=>'required|unique:users',
             'address'=>'required',
             'password'=>'required',
             'status'=>'required'
@@ -54,8 +54,8 @@ class StoreController extends Controller
     public function update(Request $request ,$id){
         $request->validate([
             'name'=>'required',
-            'email'=>'required',
-            'mobile'=>'required',
+            'email'=>'required|unique:users',
+            'mobile'=>'required|unique:users',
             'address'=>'required',
             //'password'=>'required',
             'status'=>'required'
