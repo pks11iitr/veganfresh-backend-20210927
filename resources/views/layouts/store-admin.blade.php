69ca7d7ed8c037507b27f88ec61a33bf331420b2
pankaj sengar <pks11iitr@gmail.com>
2020-10-19 11:58:06 +0530

    permission issues partilly done

--- resources/views/layouts/store-admin.blade.php
+++ resources/views/layouts/store-admin.blade.php
@@ -721,10 +721,11 @@
 
     <!-- this is for validation errors -->
         @if ($errors->any())
-            <?php var_dump($errors); ?>
             <div class="alert alert-danger">
                 <button type="button" class="close" data-dismiss="alert">×</button>
-                Please check the form below for errors
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
             </div>
         @endif
     </div>
