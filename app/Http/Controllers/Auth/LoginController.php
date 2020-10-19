d73efb307d427fddd9ddb0fcb537f8c2f4121cbb
pankaj sengar <pks11iitr@gmail.com>
2020-10-19 13:26:05 +0530

    active inactive dashboard users

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -40,6 +43,46 @@ class LoginController extends Controller
     public function redirectPath()
     {
         return route('user.role.check');
+    }
+
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $user=$this->getUser($request->email);
+
+        if(!$user || !$user->status){
+            Validator::make(['email'=>'inactive'], [
+                'email' => 'required|in:active',
+            ], ['email.*'=>'No Active Account Found'])->validate();
+        }
+
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    public function getUser($email){
+        return User::where('email', $email)->first();
     }
 
 }
