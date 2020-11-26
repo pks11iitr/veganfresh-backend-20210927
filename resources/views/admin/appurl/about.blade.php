44ac432b743e4da9ee1e2ad4066db5e18d221041
ravi <ravisaini9900@gmail.com>
2020-11-26 19:16:25 +0530

    url content

--- resources/views/admin/appurl/about.blade.php
+++ resources/views/admin/appurl/about.blade.php
@@ -1,1 +1,22 @@
-about us
+<html>
+<head>
+</head>
+<body>
+<div style="margin-left:15px;">
+
+    <h4><u>About Us</u></h4>
+    <div>HalloBasket App is focused on online grocery retail business through Application . It plans to connect household customers with our application for convenient shopping experience of grocery and daily household items. HalloBasket App is an Application available 24 X 7 to the internet friendly customers in both Android and IOS version. It is a virtual online market place facilitating easy buying of groceries. HalloBasket App aims to tap the growing Indian consumer goods market. HalloBasket App plans to start with its presence in Greater Noida ,Uttar Pradesh and then increase its regional base to Delhi NCR.</div><br>
+
+    <div>HalloBasket App aims to be the nation's leading destination for online grocery shopping. HalloBasket App connects users with us through B2C engagements. Our mission is to provide HalloBasket App users with fast, convenient delivery without ever having to leave their home.</div><br>
+
+    <div>For customers, HalloBasket App will revolutionize grocery shopping experience. Our users no longer have to wait on hold, repeat their addresses or worry about getting wrong orders. Instead, My Grocery Needs users can order exactly what they want, whenever they want with just a few clicks at their Mobile Phones.</div><br>
+
+    <div>Ecommerce market continues to grow and the amount of online purchases shows no sign of decline.Despite the continued growth of the channel, online retailers face  several challenges to growth: In our Area of Operation ,Online stores are broadly perceived as a second choice for shoppers, online retail is becoming increasingly seasonal, and online shoppers rarely admit to browsing due to unavailability of their needs at one stop destination and high delivery costs.</div><br>
+
+    <div>HalloBasket App aims to be the nation's leading destination for online grocery shopping. Our mission is to provide HalloBasket App users with fast, convenient delivery without ever having to leave their home through both Android and IOS Version.</div>
+</div>
+</body>
+</html>
+
+
+
