4fca2de8c30ebd03bc215cfadc5166f7540bc068
ravi <ravisaini9900@gmail.com>
2020-12-03 13:25:21 +0530

    privacy policy & terms

--- resources/views/admin/appurl/privacy_policy.blade.php
+++ resources/views/admin/appurl/privacy_policy.blade.php
@@ -1,1 +1,39 @@
-privacy policy
+<html>
+<head>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 20px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<div style="margin-left:15px;">
+
+    <h4><u>PRIVACY POLICY</u></h4>
+    <ul style="list-style-type:circle;">
+        <li>Products marked as “non- returnable” on the product detail page cannot be returned.</li>
+        <li>Products are returnable within the applicable return window if you have received them in a physically damaged condition , has missing parts or accessories ,defective or different from their description on the detail page on HalloBasket..</li>
+        <li>Perishable products are eligible for return only within 2 hours of time of delivery. After that no returns will be entertained for perishable products.</li>
+        <li>Some of the products referred hereby are- Dairy Products and eggs, Fruits & Vegetables, Salad products etc.</li>
+        <li>Products may be eligible for return only once in some cases, including cases of order of incorrect model/brand, incorrect colour of product ordered, incorrect quantity or incorrect product ordered.</li>
+        <li>If the replacement request is placed and our company does not have the exact same product in stock, we will provide a refund.</li>
+
+        <li>Refund, if any, shall be made at the same issuing bank through which the Product was purchased.</li>
+        <li>For payments made through electronic means like debit card, credit card, net banking, wallet etc. refund shall be made using the same payment mode.</li>
+
+        <li>For Pay  on delivery transactions, the User has the option to receive the refund as HalloBasket Cash in the HalloBasket wallet or the refund can be credited in any bank account via NEFT (for which the User shall have to share the bank details).</li>
+        <li>All refunds shall be made in Indian Rupees only.</li>
+        <li>For fulfilled by HalloBasket items ,return of delivery charges upto Rs.75 will be refunded. The cost of gift-wrapping will also be refunded, if any. All such refunds will be made to HalloBasket Wallet.</li>
+    </ul>
+</div>
+</body>
+</html>
+
+
+
+
