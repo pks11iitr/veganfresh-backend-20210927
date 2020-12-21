0ae117dacd682cef1aedc46ee5979fdab36351b2
pankaj sengar <pks11iitr@gmail.com>
2020-12-21 18:12:47 +0530

    minor fixes

--- app/Http/Controllers/Customer/Api/ProfileController.php
+++ app/Http/Controllers/Customer/Api/ProfileController.php
@@ -57,7 +57,7 @@ class ProfileController extends Controller
 
         }
 
-        if($user->update($request->only('name','email', 'dob', 'address', 'city', 'state','pincode', 'last_name'))){
+        if($user->update($request->only('name','email', 'dob', 'address', 'city', 'state','pincode', 'last_name', 'area_id'))){
             return [
                 'status'=>'success',
                 'message'=>'Profile has been updated'
