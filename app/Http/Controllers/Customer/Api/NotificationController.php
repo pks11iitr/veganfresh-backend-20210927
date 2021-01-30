dd261594762eb00d4c4fd2d2aa8d72658c64794d
pankaj sengar <pks11iitr@gmail.com>
2021-01-30 13:05:18 +0530

    adding purchase items in subadmin pemissions

--- app/Http/Controllers/Customer/Api/NotificationController.php
+++ app/Http/Controllers/Customer/Api/NotificationController.php
@@ -12,10 +12,15 @@ class NotificationController extends Controller
 
         $user=auth()->guard('customerapi')->user();
         if(!$user){
-            $notifications=Notification::where('type', 'all')->select('title', 'description', 'created_at')->orderBy('created_at', 'desc')->get();
+            $notifications=Notification::where('type', 'all')
+                ->select('title', 'description', 'created_at')
+                ->orderBy('created_at', 'desc')->get();
 
         }else{
-            $notifications=Notification::where('user_id', $user->id)->orWhere('type', 'all')->select('title', 'description', 'created_at')->orderBy('created_at', 'desc')->get();
+            $notifications=Notification::where('user_id', $user->id)
+                ->orWhere('type', 'all')
+                ->select('title', 'description', 'created_at')
+                ->orderBy('created_at', 'desc')->get();
         }
 
         return [
