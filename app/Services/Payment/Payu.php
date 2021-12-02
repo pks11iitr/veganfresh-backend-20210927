8f3f654963405a3afc9b806d974189701d76d44e
pankaj sengar <pks11iitr@gmail.com>
2021-12-02 14:18:01 +0530

    paymnet and sms gateway changed

--- app/Services/Payment/Payu.php
+++ app/Services/Payment/Payu.php
@@ -19,7 +19,7 @@ class Payu
 
     public function generateHash($data){
         //var_dump($data);die;
-        $hashSequence = $this->key.'|'.$data['refid'].'|'.$data['amount'].'|'.$data['product'].'|'.$data['name'].'|'.$data['email'].'|||||||||||'.$this->api_salt; 
+        $hashSequence = $this->key.'|'.$data['refid'].'|'.$data['amount'].'|'.$data['product'].'|'.$data['name'].'|'.$data['email'].'|||||||||||'.$this->api_salt;
         $hash = hash("sha512", $hashSequence);
         return $hash;
     }
@@ -31,19 +31,19 @@ class Payu
         return $hash;
     }
 
-    
-//genrate hash for rechage
 
-    public function generateHash_recharge($data){
-        //var_dump($data);die; 
-        $hashSequence = $this->key.'|'.$data['id'].'|'.$data['mobile'].'|'.$data['receipt'].'|'.'|'.$data['currency'].'|'.'|'.$data['amount'].'|'.$data['product'].'|'.$data['name'].'|'.$data['email'].'|||||||||||'.$this->api_salt; 
-        $hash = hash("sha512", $hashSequence);
-        return $hash;
-
-
-
-       
-    }
+//    //genrate hash for rechage
+//
+//    public function generateHash_recharge($data){
+//        //var_dump($data);die;
+//        $hashSequence = $this->key.'|'.$data['id'].'|'.$data['mobile'].'|'.$data['receipt'].'|'.'|'.$data['currency'].'|'.'|'.$data['amount'].'|'.$data['product'].'|'.$data['name'].'|'.$data['email'].'|||||||||||'.$this->api_salt;
+//        $hash = hash("sha512", $hashSequence);
+//        return $hash;
+//
+//
+//
+//
+//    }
 
 
 
